1ed7baff8248bf1957b0f1fecee63cde1c53ef09
Anthony_Araneta <anthony-araneta@MacBooks-MacBook-Pro.local>
2026-05-18 14:53:17 +0800

    user timezone

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password', 'firebase_uid'])]
+#[Fillable(['name', 'email', 'password', 'firebase_uid', 'timezone'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
